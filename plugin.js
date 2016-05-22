a1fe8baa53d416a89ecbac65de4c38f67d94f2b6
Matthijs van Henten <matthijs+foo@c9.io>
2016-05-22 22:12:00 +0000

    initial plugin version

---
+++ plugin.js
@@ -0,0 +1,80 @@
+define(function(require, exports, module) {
+    "use strict";
+    
+    main.consumes = [
+        "Plugin", "commands", "proc", "dialog.alert", "util"
+    ];
+
+    main.provides = ["command.pipe"];
+    return main;
+
+    function main(options, imports, register) {
+        var Plugin = imports.Plugin;
+
+        var plugin = new Plugin("Ajax.org", main.consumes);
+        var Range = require("ace/range").Range;
+        var commands = imports.commands;
+        var execFile = imports.proc.execFile;
+        var Alert = imports["dialog.alert"];
+        var format = require("util").format;
+        var loaded = false;
+
+        function formatCode(editor) {
+            var ace = editor.ace;
+            var sel = ace.selection;
+            var session = ace.session;
+            var range = sel.getRange();
+
+            var value = session.getTextRange(range);
+            var cmd = prompt("enter a command");
+
+            function handleError() {
+                Alert.show("Command execution failed",
+                    format("Could not execute '%s'", cmd)
+                );
+            }
+
+            execFile("~/workspace/echo.js", {
+                args: [cmd, value]
+            }, function(err, stdout, stderr) {
+                if (err) return handleError(err);
+                if (stderr) return handleError(stderr);
+
+                value = stdout;
+                var end = session.diffAndReplace(range, value);
+                sel.setSelectionRange(Range.fromPoints(range.start, end));
+            });
+
+            return true;
+        }
+
+
+        function load() {
+            loaded = true;
+
+            commands.addCommand({
+                name: "pipe_format",
+                bindKey: {
+                    mac: "Command-Shift-\\",
+                    win: "Ctrl-Shift-\\"
+                },
+                isAvailable: function(editor) {
+                    return loaded;
+                },
+                exec: function(editor) {
+                    formatCode(editor);
+                }
+            }, plugin);
+        }
+
+        plugin.on("load", load);
+
+        plugin.on("unload", function() {
+            loaded = false;
+        });
+
+        register(null, {
+            "command.pipe": plugin
+        });
+    }
+});
